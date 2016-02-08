2d718b3aa961a65cb9210692cdb05cc720173b17
lukee <luke.edwards05@gmail.com>
2016-02-08 10:12:25 -0800

    attach & initialize service worker

--- src/main.js
+++ src/main.js
@@ -1,7 +1,16 @@
 import angular from 'angular';
 import 'angular-component';
+import {isOk, onSuccess, onError} from './sw';
 import './app';
 
+// Run the service worker if we determine it's safe to
+// Note: this will Error during development because
+// 'service-worker.js' only exists after production builds.
+if (isOk) {
+	navigator.serviceWorker.register('service-worker.js').then(onSuccess).catch(onError);
+}
+
+// Custom JS goes here!
 document.addEventListener('DOMContentLoaded', () => {
 	angular.bootstrap(document, ['app']);
 });
